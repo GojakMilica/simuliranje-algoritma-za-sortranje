2404c1194b4b882a7b9fcfa1dea9f1db2bdaa6c3
GojakMilica <>
2021-05-23 20:15:14 +0200

    dodat user interface

--- sort.py
+++ sort.py
@@ -1,9 +1,12 @@
 import pygame
+from numpy import random
+import tkinter as tk
+from tkinter import simpledialog
 pygame.font.init()
 class App:
   def __init__(self):
     self.background_colour = (255,255,255)
-    self.kockica = 50
+    self.kockica = 48
     (self.width, self.height) = (20*self.kockica, 10*self.kockica)
     self.number_font = pygame.font.SysFont( 'Comic Sans MS', 16 )
 
@@ -14,22 +17,22 @@ class Clan:
     self.vrednost = vrednost
 
   def idiDole(self, brojevi,app):
-    for i in range(0,2):
-      self.top += app.kockica
+    for i in range(0,16*2):
+      self.top += app.kockica  //16
       nacrtajClanove(brojevi,app)
    
 
   def idiGore(self, brojevi,app):
-    for i in range(0,2):
-      self.top -= app.kockica
+    for i in range(0,16*2):
+      self.top -= app.kockica //16
       nacrtajClanove(brojevi,app)
 
     
   #pomeraj = za koliko treba da se pomeri
   def idiLevo(self, indeks, brojevi, app, pomeraj):
-    for i in range(0, pomeraj):
-      brojevi[indeks+pomeraj-i].left += 2*app.kockica   
-      brojevi[indeks].left -= 2*app.kockica    
+    for i in range(0, 16*pomeraj):
+      brojevi[indeks+pomeraj-i//16].left += 2*app.kockica //16  
+      brojevi[indeks].left -= 2*app.kockica //16
       nacrtajClanove(brojevi,app)
 
 
@@ -46,7 +49,7 @@ def nacrtajClanove(brojevi, app):
     number_image = app.number_font.render(str(clan.vrednost), False, (0, 0, 0))
     screen.blit( number_image, ( clan.left + app.kockica//2, clan.top + app.kockica//2 ) )
   pygame.display.update()
-  pygame.time.delay(300)
+  pygame.time.delay(10)
 
  
 def zameni(brojevi,app,pomeraj,indeks):
@@ -55,27 +58,27 @@ def zameni(brojevi,app,pomeraj,indeks):
   brojevi[indeks].idiGore(brojevi, app)
 
 
-def b1Doleb2Gore(b1,b2, app):
-  for i in range (2):
-    b1.top += app.kockica
-    b2.top -= app.kockica
+def b1Doleb2Gore(b1,b2, app, brojevi):
+  for i in range (16*2):
+    b1.top += app.kockica//16
+    b2.top -= app.kockica//16
     nacrtajClanove(brojevi, app)
 
-def idiZameniPozicije(b1, b2, pomeraj, app):
-  b1Doleb2Gore(b1,b2,app)
-  for i in range(0, pomeraj):
-      b2.left += 2*app.kockica
-      b1.left -= 2*app.kockica 
+def idiZameniPozicije(b1, b2, pomeraj, app, brojevi):
+  b1Doleb2Gore(b1,b2,app, brojevi)
+  for i in range(0, 16*pomeraj):
+      b2.left += 2*app.kockica //16
+      b1.left -= 2*app.kockica //16
       nacrtajClanove(brojevi, app)
-  b1Doleb2Gore(b2,b1,app)
+  b1Doleb2Gore(b2,b1,app, brojevi)
 
 
-def definisiNiz(arr, app):
+def definisiNiz(n, app):
   brojevi = []
-  for i in range(0, len(arr)-1):
+  for i in range(0, n):
     left = 40+i*2*app.kockica
     top = 100
-    brojevi.append( Clan(left, top, arr[i]))
+    brojevi.append( Clan(left, top, random.randint(100)))
   return brojevi
 
 def insertionSort(brojevi, app):
@@ -89,8 +92,7 @@ def insertionSort(brojevi, app):
                   brojevi[j+1].dodeli(brojevi[j])
                   j -= 1
           brojevi[j+1].dodeli(key)
-          pomeraj = i-j-1
-          zameni(brojevi,app,i-j-1,j+1)
+          if (i-j-1 != 0): zameni(brojevi,app,i-j-1,j+1)
 
 
 def bubbleSort(brojevi, app):
@@ -127,7 +129,7 @@ def selectionSort(brojevi, app):
       t.dodeli(brojevi[i])
       brojevi[i].dodeli(brojevi[min_idx])
       brojevi[min_idx].dodeli(t)
-      if (min_idx-i != 0): idiZameniPozicije(brojevi[i], brojevi[min_idx], min_idx-i, app)
+      if (min_idx-i != 0): idiZameniPozicije(brojevi[i], brojevi[min_idx], min_idx-i, app, brojevi)
 
 def shellSort(brojevi, app):
     t = Clan(0,0,0)
@@ -145,18 +147,38 @@ def shellSort(brojevi, app):
             if (i != j): idiZameniPozicije(brojevi[j], t, interval, app)
         interval //= 2
 
-    
+def popups():
+  ROOT = tk.Tk()
 
-arr = [9,5,3,8,6,4,1,2,7,10]
+  ROOT.withdraw()
+  USER_INP = simpledialog.askstring(title = "br clanova", prompt="Unesite broj clanova")
+  n = int(USER_INP)
+
+  USER_INP2 = simpledialog.askstring(title = "sort", prompt="Koji sort zelite?(insertion, selection, bubble)")
+  x = str(USER_INP2)
+  return n, x
+
+def main(n, x, app):
+    brojevi = definisiNiz(n, app)
+    nacrtajClanove(brojevi, app)
+    if (x is "insertion"): insertionSort(brojevi, app)
+    elif (x is"selection"): selectionSort(brojevi, app)
+    elif (x is "bubble"): bubbleSort(brojevi, app)
+
+
+
 app = App()
-brojevi = definisiNiz(arr, app)
+n, x = popups()
 screen = pygame.display.set_mode((app.width, app.height))
-pygame.display.set_caption('insertion')
+pygame.display.set_caption('simulacija sorta')
 screen.fill(app.background_colour)
-
-
+brojevi = definisiNiz(n, app)
 nacrtajClanove(brojevi, app)
-selectionSort(brojevi, app)
+if (x == "insertion"): insertionSort(brojevi, app)
+elif (x =="selection"): selectionSort(brojevi, app)
+elif (x == "bubble"): bubbleSort(brojevi, app)
+
+
 pygame.display.update() 
 running = True
 while running:
